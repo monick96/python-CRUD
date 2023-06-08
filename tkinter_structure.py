3caf4cd08eea94683e3ccefb931a9f1295b68267
monica <monibrave1@gmail.com>
2023-06-08 18:36:31 -0300

    added all functions

--- tkinter_structure.py
+++ tkinter_structure.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
 import sqlite3 as sq3 
-
+import matplotlib.pyplot as plt
 
 '''
 *********************************
@@ -9,6 +9,26 @@ import sqlite3 as sq3
 *********************************
 '''
 # *********** FUNCIONES VARIAS **************
+def buscar_escuelas(actualiza):
+    con = sq3.connect('my-db.db')
+    cur = con.cursor()
+    if actualiza: 
+        cur.execute('SELECT _id, localidad, provincia FROM escuelas WHERE nombre =?',(escuela.get(),)) #or ...WHERE nombre =' + escuela.get())
+    else:#Acá configuro opción para llenar la lista de escuelas cuando abro CRUD
+        cur.execute("SELECT nombre FROM escuelas")
+    
+    resultado = cur.fetchall() #RECIBO LISTA DE TUPLAS CON UN ELEMENTO "FANTASMA"
+    #print(resultado)
+    retorno = []
+    for e in resultado:
+        if actualiza:
+            provincia.set(e[2])
+            localidad.set(e[1])
+        esc = e[0] #id
+        retorno.append(esc)
+        
+    con.close()
+    return retorno
 
 
 # *********** MENU **************
@@ -23,6 +43,53 @@ def conectar():
     cur = con.cursor()
     messagebox.showinfo("STATUS","Conectado con exito")
 #       Listar
+def listar():
+    class Table():
+        def __init__(self,ubicacion):
+            nombre_cols = ['Legajo', 'Apellido','Nombre','Promedio', 'Email', 
+            'Escuela', 'Localidad', 'Provincia']
+            for i in range(cant_cols):
+                self.e = Entry(ubicacion)
+                self.e.config(bg='black', fg='white')
+                self.e.grid(row=0, column=i)
+                self.e.insert(END,nombre_cols[i])
+
+            for fila in range(cant_filas):
+                for col in range(cant_cols):
+                    self.e = Entry(ubicacion)
+                    self.e.grid(row=fila+1, column=col)
+                    self.e.insert(END, resultado[fila][col])
+                    self.e.config(state='readonly')
+
+    raiz2 = Tk()
+    raiz2.title('Listado alumnos')
+    frameppal = Frame(raiz2)    
+    frameppal.pack(fill='both')
+    framecerrar = Frame(raiz2)
+    framecerrar.config(bg=color_texto_boton)
+    framecerrar.pack(fill='both')
+
+    boton_cerrar = Button(framecerrar,text="CERRAR", command=raiz2.destroy)
+    boton_cerrar.config(bg=color_fondo_boton, fg=color_texto_boton, pady=10, padx=0)
+    boton_cerrar.pack(fill='both')
+
+    # obtengo los datos 
+    con = sq3.connect('my-db.db')
+    cur = con.cursor()
+    query1 = '''
+            SELECT alumnos.legajo, alumnos.apellido, alumnos.nombre, alumnos.nota, alumnos.email, 
+            escuelas.nombre, escuelas.localidad, escuelas.provincia
+            FROM alumnos INNER JOIN escuelas
+            ON alumnos.id_escuela = escuelas._id LIMIT 30
+            '''
+    cur.execute(query1)
+    resultado = cur.fetchall()
+    cant_filas = len(resultado) # la cantidad de registros para saber cuántas filas
+    cant_cols = len(resultado[0]) # obtengo la cantidad de columnas
+    
+    tabla = Table(frameppal)
+    con.close()
+    raiz2.mainloop()
 '''----------------------------------------------------------------'''
 #       Salir
 def salir():
@@ -32,6 +99,55 @@ def salir():
         raiz.destroy()
 
 #   GRÁFICAS
+    #Alumnos por escuela
+def alumnos_por_escuela():
+    query_buscar = '''SELECT COUNT(alumnos.legajo) AS total, escuelas.nombre FROM
+    alumnos INNER JOIN escuelas 
+    ON alumnos.id_escuela=escuelas._id
+    GROUP BY escuelas.nombre
+    ORDER BY total DESC'''
+    cur.execute(query_buscar)
+    resultado = cur.fetchall()
+
+    cantalu=[]
+    escuelas=[]
+
+    for i in resultado:
+        cantalu.append(i[0])
+        escuelas.append(i[1])
+
+    plt.bar(escuelas,cantalu)
+    plt.xticks(rotation=45)
+    plt.show()
+
+    #Promedio por escuela
+
+def promedio_notas():
+    query_buscar ='''SELECT AVG(alumnos.nota) AS "promedio", escuelas.nombre 
+    FROM alumnos INNER JOIN escuelas
+    ON alumnos.id_escuela = escuelas._id 
+    GROUP BY escuelas.nombre 
+    ORDER BY promedio'''
+    cur.execute(query_buscar)
+    resultado = cur.fetchall()
+    
+    promedio = []
+    school = []
+    for i in resultado:
+        promedio.append(i[0])
+        school.append(i[1])
+    print(promedio)
+    print(school)
+
+    plt.barh(school,promedio,height=0.5)
+    
+    #Iterando los valores de promedio con función enumarate para mostrar en barras
+    for index, value in enumerate(promedio):
+        plt.text(value, index,
+                    round(float(value),2))
+    
+    plt.show()
+
 
 #   LIMPIAR
 def limpiar():
@@ -73,9 +189,56 @@ def mostrar_acercade():
 # *********** MENU CRUD (CREATE-READ-UPDATE-DELETE**************
 
 #       CREAR
+def crear():
+    id_escuela=int(buscar_escuelas(True)[0])
+    datos = id_escuela, legajo.get(),apellido.get(),nombre.get(),calificacion.get(),email.get()
+    cur.execute("INSERT INTO alumnos (id_escuela, legajo, apellido,	nombre, nota, email) VALUES (?,?,?,?,?,?)",datos)
+    con.commit()
+    messagebox.showinfo("Status","Registro agregado")
+    limpiar()
 #       LEER
+def buscar_legajo():
+    query_buscar = '''SELECT a.legajo, a.apellido, a.nombre, a.email, a.nota, 
+    e.nombre, e.localidad, e.provincia 
+    FROM alumnos a INNER JOIN 
+    escuelas e ON a.id_escuela=e._id 
+    WHERE a.legajo='''
+    cur.execute(query_buscar + legajo.get())
+    
+    resultado = cur.fetchall() #trae lista de tuplas
+    if resultado == []:
+        messagebox.showerror('No encontrado','No existe numero de legajo')
+    else:
+        for campo in resultado:
+            legajo.set(campo[0])
+            apellido.set(campo[1])  
+            nombre.set(campo[2])
+            email.set(campo[3])  
+            calificacion.set(campo[4])  
+            escuela.set(campo[5])  
+            localidad.set(campo[6])  
+            provincia.set(campo[7])
+            legajo_input.config(state='disabled')
+    
+    
 #       ACTUALIZAR
+def actualizar():
+    id_escuela=int(buscar_escuelas(True)[0])
+    datos2 = id_escuela, apellido.get(),nombre.get(),calificacion.get(),email.get()
+    cur.execute("UPDATE alumnos set id_escuela=?, apellido=?,nombre=?, nota=?, email=? WHERE legajo = " + legajo.get(),datos2)
+    con.commit()
+    messagebox.showinfo("Status","Registro actualizado")
+    limpiar()
+    
+    
 #       BORRAR
+def borrar():
+    resp = messagebox.askquestion("Confirme","¿Realmente querés borrar registro?")
+    if resp == "yes":
+        cur.execute("DELETE FROM alumnos WHERE legajo=" + legajo.get())
+        con.commit()
+        messagebox.showinfo("Status","Registro eliminado correctamente")
+        limpiar()
 
 
 '''
@@ -106,7 +269,7 @@ bbddmenu = Menu(barramenu,tearoff=0) #objeto de clase barramenu
 #conect button
 bbddmenu.add_command(label="Conect BBDD",command=conectar)
 #student list button
-bbddmenu.add_command(label="List of students")
+bbddmenu.add_command(label="List of students",command=listar)
 #exit button
 bbddmenu.add_command(label="Exit",command=salir)
 
@@ -114,10 +277,10 @@ bbddmenu.add_command(label="Exit",command=salir)
 ######SUB MENU Graficas######
 statmenu= Menu(barramenu,tearoff=0) #objeto de clase barramenu
     #comandos SUB MENU Graficas
-#students per school
-statmenu.add_command(label="Students per school")
+#students per school button to do grafica
+statmenu.add_command(label="Students per school",command=alumnos_por_escuela)
 #student score
-statmenu.add_command(label="Score")
+statmenu.add_command(label="Score",command=promedio_notas)
 
 ######SUB MENU Limpiar######
 clearmenu=Menu(barramenu,tearoff=0)
@@ -213,12 +376,13 @@ apellido_input = Entry(framefields, textvariable=apellido)
 nombre_input = Entry(framefields, textvariable=nombre)
 email_input=Entry(framefields, textvariable=email)
 calificacion_input=Entry(framefields, textvariable=calificacion)
-'''
+
+#lista desplegable
 escuelas = buscar_escuelas(False)
 escuela.set('Seleccione')
 escuela_option = OptionMenu(framefields, escuela,*escuelas)
 escuela_option.grid(row=5, column=1, padx=10, pady=10, sticky='w', ipadx=50)
-'''
+
 
 localidad_input=Entry(framefields, textvariable=localidad)
 localidad_input.config(state='readonly')
@@ -244,13 +408,13 @@ def config_buttons(mi_button, columna):
     mi_button.config(bg=color_fondo_boton, fg=color_texto_boton)
     mi_button.grid(column = columna, **espaciado_buttons)
 
-boton_crear = Button(framebotones, text='Crear')
+boton_crear = Button(framebotones, text='Crear',command=crear)
 config_buttons(boton_crear, 0)
 
-boton_buscar = Button(framebotones, text='Buscar')
+boton_buscar = Button(framebotones, text='Buscar',command=buscar_legajo)
 config_buttons(boton_buscar, 1)
 
-boton_actualizar =Button(framebotones, text='Actualizar')
+boton_actualizar =Button(framebotones, text='Actualizar',command=actualizar)
 config_buttons(boton_actualizar, 2)
 
 boton_borrar = Button(framebotones, text='Eliminar')
